cf549b6b5224ef2c95b19f35c115661723480d8d
Barend Garvelink <bgarvelink@xebia.com>
2013-05-27 22:17:33 +0200

    Default empty project from command line.

---
+++ src/com/xebia/xebicon2013/cciaa/AddressListActivity.java
@@ -0,0 +1,15 @@
+package com.xebia.xebicon2013.cciaa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddressListActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
